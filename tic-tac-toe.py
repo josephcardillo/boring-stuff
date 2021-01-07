dc5fa6e458f15f6e49f95465f84198ad79b2362e
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2021-01-07 16:51:59 -0500

    tic-tac-toe updates

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,27 +6,35 @@ game_board_positions = {'TL': 'TL', 'TM': 'TM', 'TR': 'TR',
                         'ML': 'ML', 'MM': 'MM', 'MR': 'MR',
                         'BL': 'BL', 'BM': 'BM', 'BR': 'BR'}
 
-play = input("Hello. Would you like to play Tic-Tac-Toe? Y or N > ")
+play = input("Hello. Would you like to play Tic-Tac-Toe? Y or N > ").upper()
 
 def print_board(board):
+    print()
     print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
-    print('-+-+-')
+    if board['TL'] == 'TL':
+        print('--+--+--')
+    else:
+        print('-+-+-')
     print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
-    print('-+-+-')
+    if board['TL'] == 'TL':
+        print('--+--+--')
+    else:
+        print('-+-+-')
     print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
+    print()
 
 def init_players():
     player1_pick = ' '
     while player1_pick != 'X' and player1_pick != 'O':
-        player1_pick = input("Player 1, choose X or O > ")
+        player1_pick = input("Player 1, choose X or O > ").upper()
     print(f"Player 1 is > {player1_pick}")
-    player1_name = input("Player 1 name > ")
+    player1_name = input("Player 1 name > ").upper()
     if player1_pick == 'X':
         player2_pick = 'O'
     else:
         player2_pick = 'X'
     print(f"Player 2 is > {player2_pick}")
-    player2_name = input("Player 2 name > ")
+    player2_name = input("Player 2 name > ").upper()
     return player1_pick, player1_name, player2_pick, player2_name
 
 if play == 'Y':
@@ -78,10 +86,10 @@ def check_win():
 
 for i in range(9):
     if i % 2 == 0:
-        player1_move = input(f"{players[1]} choose your move > ")
+        player1_move = input(f"{players[1]} choose your move > ").upper()
         while game_board[player1_move] != ' ':
             print(f"Can't play at {player1_move}")
-            player1_move = input(f"{players[1]} choose your move > ")
+            player1_move = input(f"{players[1]} choose your move > ").upper()
         print(f"Move is > {player1_move}")
         game_board[player1_move] = players[0]
         print_board(game_board)
@@ -89,10 +97,10 @@ for i in range(9):
             print(f"{players[1]} wins!")
             break
     else:
-        player2_move = input(f"{players[3]} choose your move > ")
+        player2_move = input(f"{players[3]} choose your move > ").upper()
         while game_board[player2_move] != ' ':
             print(f"Can't play at {player2_move}")
-            player2_move = input(f"{players[3]} choose your move > ")
+            player2_move = input(f"{players[3]} choose your move > ").upper()
         print(f"Move is > {player2_move}")
         game_board[player2_move] = players[2]
         print_board(game_board)
