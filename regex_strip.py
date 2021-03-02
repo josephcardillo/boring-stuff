e1b0ff5a62798acf374bdd313be694974e2b2f4c
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2021-03-02 11:51:38 -0500

    regex strip exercise

--- regex_strip.py
+++ regex_strip.py
@@ -5,7 +5,21 @@ import re
 
 def regex_strip_space(string, chars_to_remove=''):
     # IN PROGRESS
-    strip_space_regex = re.compile(f'(\s+)(\w+)([{chars_to_remove}])(\s+)')
-    stripped_string = strip_space_regex.findall(string)[0][1]
-    return stripped_string
-
+    if string[0] == ' ' and string[-1] != ' ':
+        strip_space_regex = re.compile(f'^\s+')
+        stripped_string = strip_space_regex.sub('', string)
+        return stripped_string
+    elif string[-1] == ' ' and string[0] != ' ':
+        strip_space_regex = re.compile(f'(\w+)(\s+)')
+        stripped_string = strip_space_regex.findall(string)[0][0]
+        return stripped_string
+    elif string[0] == ' ' and string[-1] == ' ':
+        strip_space_regex = re.compile(f'(\s+)(\w+)(\s+)')
+        return stripped_string[0][1]
+    elif chars_to_remove != '':
+        strip_space_regex = re.compile(f'[{chars_to_remove}]')
+        stripped_string = strip_space_regex.sub('', string)
+        return stripped_string
+    else:
+        pass
+    
